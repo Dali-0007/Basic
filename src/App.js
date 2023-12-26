169832f023f41da47b91e1c334ffbf737d259f8e
Dali Naidu Arnipalli <adali@pkrmsezdc.com>
2023-12-26 11:36:36 +0530

    secondcommit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>heaiasouf gfoidlsf</p>
+        <h3>Naidu</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
